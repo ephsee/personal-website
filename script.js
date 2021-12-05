c94e1d5e14b441113d786e3f7be90067d0301552
Francois Spies <fcspies@gmail.com>
2021-12-05 18:49:38 -0500

    Project Completed

---
+++ script.js
@@ -0,0 +1,7 @@
+
+for (const target of
+  document.getElementsByClassName("bullseye"))
+  target.addEventListener("click", alertMeessage);
+  function alertMeessage() {
+     alert("Yee-Haw... BULLSEYE!!! Great Shootin' Tex!");
+  }
